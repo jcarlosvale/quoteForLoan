4d2c1ec59514f31d0bb73303e07923d050cbf653
joao.vale <joao.vale@sky.uk>
2019-08-07 14:44:55 +0100

    initial push

---
+++ src/test/java/com/zopa/rate/api/AppTest.java
@@ -0,0 +1,17 @@
+package com.zopa.rate.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
